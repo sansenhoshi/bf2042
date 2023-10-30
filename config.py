b0faa843411ed2477b99e35bcc020ed6b01574fb
ccc_dw <2931589710@qq.com>
2023-10-30 16:48:26 +0800

    配置文件说明

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import re
 
-DATABASE_NAME = "test_db"
-USERNAME = "test"  # 用户名
-PASSWORD = "test23333"  # 密码
-DATABASE_HOST = "sansenhoshi.top"  # 数据库地址
-DATABASE_PORT = 3306   # 数据库端口
+DATABASE_NAME = "test_db"           # 数据库名
+USERNAME = "test"                   # 用户名
+PASSWORD = "test23333"              # 密码
+DATABASE_HOST = "sansenhoshi.top"   # 数据库地址
+DATABASE_PORT = 3306                # 数据库端口
+
+# 需要同步绑定数据库的请联系QQ：2931589710
